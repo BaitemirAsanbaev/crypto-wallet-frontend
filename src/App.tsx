29946dcdee297f13c6f6bab1dcb6b9fffb52303b
AsanbaevBaitemir <baitemir1706@gmail.com>
2024-05-28 17:59:23 +0600

    init project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import {Sidebar} from "./components/Sidebar.tsx";
+import {Route, RouterProvider, Routes} from "react-router-dom";
+import React from "react";
+import {HomePage} from "./pages/home-page.tsx";
+import {ProfilePage} from "./pages/profile-page.tsx";
+import {NotFoundPage} from "./pages/not-found-page.tsx";
+
+export const App = () => {
+    return <>
+        <Sidebar/>
+        <Routes>
+            <Route index path={'/'} element={<HomePage/>}/>
+            <Route path={'/profile'} element={<ProfilePage/>}/>
+            <Route path={'*'} element={<NotFoundPage/>}/>
+        </Routes>
+    </>
+}
